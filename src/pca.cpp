761115d66602e23a48abc37101a044ac0381f271
fourofclubs <lucasvitali001@gmail.com>
2021-09-30 17:00:00 -0300

    PCA almost ready

--- src/pca.cpp
+++ src/pca.cpp
@@ -12,10 +12,36 @@ PCA::PCA(unsigned int n_components)
 
 void PCA::fit(Matrix X)
 {
+    // Pre: X es una matriz de n muestras por 784 columnas
+    //      que representan cada imagen
+
+    //X_u = (X - medias)/raiz(n-1)
+    _mean = X.colwise().mean();
+    _n = X.rows();
+
+    Eigen::MatrixXd X_u = (X - _mean)/sqrt(_n-1);
+
+    // Calculo matriz de Covarianza M_x_u
+    Eigen::MatrixXd M_x = X_u.transpose() * X_u;
+
+    // Guardo matriz P = V^t de autovectores de M_x_u
+    // Siendo P la matriz cambio de base
+
+    // _P = matriz de autovectores de M_x;
+
 }
 
 
 MatrixXd PCA::transform(Matrix X)
 {
-  throw std::runtime_error("Sin implementar");
+    // Pre: X es una matriz de n muestras por 784 columnas
+    //      que representan cada imagen
+
+    // X^ = (X - medias(del fit)) /raiz(n-1)(del fit)
+    Eigen::MatrixXd X_p = (X - _mean)/sqrt(_n-1);
+
+    // X_p = P.X_^
+    X_p = _P * X_p;
+
+    return X_p;
 }
--- src/pca.h
+++ src/pca.h
@@ -3,10 +3,17 @@
 
 class PCA {
 public:
-    PCA(unsigned int n_components);
+    PCA(unsigned int n_components); // constructor de clase PCA
 
+    // Calcula transformacion PCA dada una muestra
     void fit(Matrix X);
 
+    // Realiza transformacion PCA de una imagen dada
     Eigen::MatrixXd transform(Matrix X);
 private:
+
+    Eigen::VectorXd _mean; // media de la muestra
+    int _n; // cantidad de imagenes en la muestra
+    Eigen::MatrixXd _P; // matriz de cambio de base
+
 };
