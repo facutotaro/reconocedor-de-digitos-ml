ecc4f8093ba867a835bf65e1f255e6e9ba7bf90a
fourofclubs <lucasvitali001@gmail.com>
2021-10-02 11:03:39 -0300

    add comment PCA

--- src/pca.cpp
+++ src/pca.cpp
@@ -19,10 +19,10 @@ void PCA::fit(Matrix X)
     _mean = X.colwise().mean();
     _n = X.rows();
 
-    Eigen::MatrixXd X_u = (X - _mean)/sqrt(_n-1);
+    Matrix X_u = (X - _mean)/sqrt(_n-1);
 
     // Calculo matriz de Covarianza M_x_u
-    Eigen::MatrixXd M_x_u = X_u.transpose() * X_u;
+    Matrix M_x_u = X_u.transpose() * X_u;
 
     // Guardo matriz P = V^t de autovectores de M_x_u
     // Siendo P la matriz cambio de base
@@ -38,10 +38,10 @@ MatrixXd PCA::transform(Matrix X)
     //      que representan cada imagen
 
     // X^ = (X - medias(del fit)) /raiz(n-1)(del fit)
-    Eigen::MatrixXd X_u = (X - _mean)/sqrt(_n-1);
+    Matrix X_u = (X - _mean)/sqrt(_n-1);
 
     // X_p = P.X_^
-    Eigen::MatrixXd X_p = _P * X_u.transpose();
+    Matrix X_p = _P * X_u.transpose(); // X_p_t -> (alpha x n) = P -> (alpha x 784) . X -> (784 x n)
 
-    return X_p.transpose();
+    return X_p.transpose(); // X_p = X_p_t.transpose()
 }
